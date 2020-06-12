b37131b087ad1e6255202b3e9b9ad51506d667fc
Dave Honneffer <pearofducks@gmail.com>
2020-06-12 13:44:04 +0200

    Vue 3

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,9 @@
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import buble from 'rollup-plugin-buble'
-
 const pkg = require('./package.json')
-const libraryName = 'VueSimpleMedia'
 const input = 'index.js'
-const sourcemap = true
 
 export default {
   input,
   output: [
-    { file: pkg.main, name: libraryName, format: 'umd', sourcemap },
-    { file: pkg.module, format: 'es', sourcemap },
+    { file: pkg.module, format: 'es' },
   ],
-  plugins: [ buble({ objectAssign: 'Object.assign' }), sourceMaps() ]
 }
